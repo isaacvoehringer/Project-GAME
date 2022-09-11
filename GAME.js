0bee8b37558f51ba83573e7ec00d56a1b545491d
isaac <voehringer.isaac@gmail.com>
2022-09-11 13:27:03 -0500

    added players still trying to get collison

--- GAME.js
+++ GAME.js
@@ -156,6 +156,78 @@ function sleep(time){
     })  
 }
 
+function newNonPlayableOffense(x, y) {
+    let element = newImage('./assets/blocker.jpeg')
+    element.style.zIndex = 1;
+    
+    let direction = null;
+
+    function moveCharacter() {
+        if (direction === 'west') {
+            x -= 1
+        }
+        if (direction === 'north') {
+            y += 1
+        }
+        if (direction === 'east') {
+            x += 1
+        }
+        if (direction === 'south') {
+            y -= 1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+
+    setInterval(moveCharacter, 1)
+
+    async function walkEast(time) {
+        direction = 'east'
+        element.src = './assets/defender.jpeg'
+        await sleep(time)
+        stop()
+    }
+
+    async function walkNorth(time) {
+        direction = 'north'
+        element.src = './assets/defender.jpeg'
+        await sleep(time)
+        stop()
+    }
+
+    async function walkWest(time) {
+        direction = 'west'
+        element.src = './assets/defender.jpeg'
+        await sleep(time)
+        stop()
+    }
+
+    async function walkSouth(time) {
+        direction = 'south'
+        element.src = './assets/defender.jpeg'
+        await sleep(time)
+        stop()
+    }
+
+    function stop() {
+        direction = null
+        element.src = './assets/defender.jpeg'
+    }
+
+    return {
+        element: element,
+        walkWest: walkWest,
+        walkNorth: walkNorth,
+        walkEast: walkEast,
+        walkSouth: walkSouth,
+        stop: stop
+    }
+}
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
 
 
 const player = newPlayableCharacter(200, 300)
@@ -164,6 +236,9 @@ const def2 = newNonPlayableCharacter(400, 400)
 const def3 = newNonPlayableCharacter(400, 300)
 const def4 = newNonPlayableCharacter(400, 200)
 
+const blocker1 = newNonPlayableOffense(200, 500)
+const blocker2 = newNonPlayableOffense(200, 400)
+const blocker3 = newNonPlayableOffense(200, 200)
 // let touchdown = document.querySelector(".endzone")
 
 // touchdown.addEventListener()
@@ -185,4 +260,19 @@ $(document).ready(function() {
     console.log(position)				
 });
 
+// let touchdown(){
+//     if x === f {console.log('touchdown')}
+// }
 
+
+async function movedef1(){
+    await def1.walkEast(300)
+    await def1.walkSouth(600)
+    await def1.walkEast(200)
+    await def1.walkSouth(200)
+    await def1.walkWest(700)
+    await def1.walkNorth(800)
+    await movedef1()
+}
+
+movedef1()
