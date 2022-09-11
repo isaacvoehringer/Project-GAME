96bf3c533788a64caeffb75f683ad5c2ee4d5a46
isaac <voehringer.isaac@gmail.com>
2022-09-11 07:58:26 -0500

    still struggling

--- GAME.js
+++ GAME.js
@@ -1,35 +1,80 @@
-function newImage(url){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'absolute'
-    document.body.append(image)
-    return image
-}
+// console.log('hi')
+
+
+
 function newPlayableCharacter(x, y) {
-    const element = newImage('./ass')
+    const element = newImage('./assets.brownietheelf.jpeg')
     element.style.zIndex = 1;
 
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/green-character/static.gif`
-        }
-        if (direction === 'west') {
-            element.src = `assets/green-character/west.gif`
-        }
-        if (direction === 'north') {
-            element.src = `assets/green-character/north.gif`
-        }
-        if (direction === 'east') {
-            element.src = `assets/green-character/east.gif`
-        }
-        if (direction === 'south') {
-            element.src = `assets/green-character/south.gif`
-        }
-    }
+ 
 
-    move(element).withArrowKeys(x, y, handleDirectionChange)
+    move(element).withArrowKeys(x, y)
 
     return {
         element: element
+    }
+}
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'right'){
+                x-=1
+            }
+            if(direction === 'up'){
+                y+=1
+            }
+            if(direction === 'left'){
+                x+=1
+            }
+            if(direction === 'down'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'left'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'up'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'right'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'down'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 It's 4th and Goal in the Super Bowl with 1 second left... The Browns are down by 4 points! They had the ball to star running back Nick Chubb. The user controls Chubb with the arrow keys and if he crosses the goal line the Browns win the superbowl, if he steps out of bounds, they lose. Collison won't matter because no1 would be able to tackle him in this situation.
 
 i will make a playing field in a portion of the screen and place the players on the field. i will need to make boundaries for the sidelines and the goalline and reactions to the user crossing the lines
+
+set up field and initail styling and image  now i need to allow arrow key functionality
