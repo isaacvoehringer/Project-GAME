d7f2e0f61da413377e3c4dd738f1e6a892834a91
isaac <voehringer.isaac@gmail.com>
2022-09-13 19:02:25 -0500

    adding a reset button

--- GAME.js
+++ GAME.js
@@ -380,9 +380,8 @@ moveblocker3()
 // {
 //     return ((r1.X + r1.Width >= r2.X) and (r1.X <= r2.X + r2.Width) and (r1.Y + r1.Height >= r2.Y) and (r1.Y <= r2.Y + r2.Height));
 // }
-function reset()
-let btn = document.getElementById('reset') {
-    btn.onclick {
-        console.log(reset)
+
+function reset(){
+let btn = document.getElementById('reset'); 
+    btn.addEventListener("click", console.log(reset))
     }
-}
